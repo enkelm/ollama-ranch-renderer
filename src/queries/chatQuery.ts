9dbd20ac08ebfab9d827fe74e073b7478b2fae49
Enkel Murati <enkel.murati33@gmail.com>
2025-05-14 21:27:20 +0200

    feat: implement chat

--- src/queries/chatQuery.ts
+++ src/queries/chatQuery.ts
@@ -3,18 +3,34 @@ import {
   queryOptions,
   experimental_streamedQuery as streamedQuery,
 } from '@tanstack/react-query'
+import type { ChatResponse } from 'ollama/browser'
 
-export const chatQueryOptionsBuilder = (model: string, prompt: string) =>
+export const chatQueryOptionsBuilder = (
+  model: string,
+  content: string | undefined,
+) =>
   queryOptions({
-    queryKey: ['chat', prompt],
+    queryKey: ['chat', model, content],
     queryFn: streamedQuery({
-      queryFn: () =>
-        ollama.generate({
-          model,
-          prompt,
-          stream: true,
-        }),
+      queryFn: async function* () {
+        if (content?.trim()) {
+          const chatStream = await ollama.chat({
+            model,
+            messages: [{ role: 'user', content }],
+            stream: true,
+          })
+
+          const id = crypto.randomUUID()
+          for await (const chunk of chatStream) {
+            yield { id, ...chunk }
+          }
+        } else {
+          return yield Promise.reject({ done: true } as ChatResponse)
+        }
+      },
+      // refetchMode: 'append',
     }),
     refetchOnMount: false,
     refetchOnWindowFocus: false,
+    staleTime: 1000 * 5,
   })
--- src/routes/chat.tsx
+++ src/routes/chat.tsx
@@ -1,8 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useQuery } from '@tanstack/react-query'
-import { memo, useActionState, useRef } from 'react'
+import {
+  memo,
+  useActionState,
+  useDeferredValue,
+  useEffect,
+  useOptimistic,
+  useRef,
+  useState,
+  useTransition,
+} from 'react'
 import { useFormStatus } from 'react-dom'
-import { Loader2Icon, LoaderIcon } from 'lucide-react'
+import { Loader2Icon } from 'lucide-react'
 import Markdown from 'react-markdown'
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
@@ -20,7 +29,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
-import { ScrollArea } from '@/components/ui/scroll-area'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export const Route = createFileRoute('/chat')({
   beforeLoad: () => ({
@@ -88,30 +97,24 @@ const ModelSelector = memo(
   },
 )
 
-function RouteComponent() {
-  const [formState, action] = useActionState<
-    { model: string; prompt: string },
-    FormData
-  >(
-    (prevState, formData) => {
-      return {
-        model: formData.get('model')?.toString() ?? prevState.model,
-        prompt: formData.get('prompt')?.toString() ?? '',
-      }
-    },
-    { model: 'qwen2.5-coder:1.5b', prompt: '' },
-  )
-
-  const chatQuery = useQuery(
-    chatQueryOptionsBuilder(formState.model, formState.prompt),
-  )
-
-  const formRef = useRef<HTMLFormElement>(null)
-
+type IChatMessage = {
+  id: `${string}-${string}-${string}-${string}-${string}`
+  role: 'assistant' | 'user'
+  content: string
+  generating: boolean
+}
+type IChatMessageProps = Omit<IChatMessage, 'id'>
+const ChatMessage = ({ generating, content, role }: IChatMessageProps) => {
+  if (!content.trim()) {
+    return null
+  }
   return (
-    <div className="h-full flex flex-col justify-between">
-      <section>
-        {chatQuery.data?.at(-1)?.done === false && (
+    <Card className="mb-4">
+      <CardHeader>
+        <CardTitle>{role === 'assistant' ? 'Assistant' : 'User'}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        {generating && (
           <span className="flex gap-2 font-bold animate-pulse">
             <Loader2Icon className="animate-spin" /> Generating...
           </span>
@@ -136,8 +139,90 @@ function RouteComponent() {
             },
           }}
         >
-          {chatQuery.data?.map((d) => d.response).join('')}
+          {content}
         </Markdown>
+      </CardContent>
+    </Card>
+  )
+}
+
+function RouteComponent() {
+  const [chat, setChat] = useState<Array<IChatMessage>>([])
+  const [formState, action] = useActionState<
+    { model: string; prompt: string },
+    FormData
+  >(
+    (prevState, formData) => {
+      const prompt = formData.get('prompt')?.toString() ?? ''
+      setChat((c) =>
+        c.concat([
+          {
+            id: crypto.randomUUID(),
+            role: 'user',
+            content: prompt,
+            generating: false,
+          },
+        ]),
+      )
+      return {
+        model: formData.get('model')?.toString() ?? prevState.model,
+        prompt: chat.map((c) => c.content) + prompt,
+      }
+    },
+    { model: 'qwen2.5-coder:1.5b', prompt: '' },
+  )
+
+  const chatQuery = useQuery(
+    chatQueryOptionsBuilder(formState.model, formState.prompt),
+  )
+
+  const chatDeferred = useDeferredValue<Array<IChatMessage>>(
+    chatQuery.isPending ||
+      !chat.find((c) => chatQuery.data?.at(-1)?.id === c.id)
+      ? chat.concat([
+          {
+            id: chatQuery.data?.at(-1)?.id ?? crypto.randomUUID(),
+            role: 'assistant',
+            content:
+              chatQuery.data
+                ?.map((d) => {
+                  return d.message.content
+                })
+                .join('') ?? '',
+            generating: !(chatQuery.data?.at(-1)?.done ?? false),
+          },
+        ])
+      : chat,
+  )
+
+  useEffect(() => {
+    if (chatQuery.data?.at(-1)?.done) {
+      setChat((c) =>
+        c.concat([
+          {
+            id: chatQuery.data.at(-1)!.id,
+            role: 'assistant',
+            content: chatDeferred.at(-1)!.content,
+            generating: false,
+          },
+        ]),
+      )
+    }
+  }, [chatQuery.data?.at(-1)?.done, chatQuery.data?.at(-1)!.id, setChat])
+
+  const formRef = useRef<HTMLFormElement>(null)
+
+  return (
+    <div className="h-full flex flex-col justify-between">
+      <section>
+        {chatDeferred.map((c) => (
+          <ChatMessage
+            key={c.id}
+            role={c.role}
+            content={c.content}
+            generating={c.generating}
+          />
+        ))}
       </section>
 
       <form ref={formRef} action={action} className="flex flex-row gap-4">
